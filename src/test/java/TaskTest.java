dd776f7dc5c7270d2cf744a97f3a1a55a3890d07
linhaomiao <linhaomiao@bluemoon.com.cn>
2020-04-12 14:21:44 +0800

    添加定时任务依赖和测试类

---
+++ src/test/java/TaskTest.java
@@ -0,0 +1,40 @@
+import com.jsh.erp.ErpApplication;
+import com.jsh.erp.schedule.CronTaskRegistrar;
+import com.jsh.erp.schedule.SchedulingRunnable;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * @program: simple-demo
+ * @description: 测试定时任务
+ * @author: CaoTing
+ * @date: 2019/5/23
+ **/
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = ErpApplication.class)
+public class TaskTest {
+
+    @Autowired
+    CronTaskRegistrar cronTaskRegistrar;
+
+    @Test
+    public void testTask() throws InterruptedException {
+        SchedulingRunnable task = new SchedulingRunnable("demoTask", "taskNoParams", null);
+        cronTaskRegistrar.addCronTask(task, "0/10 * * * * ?");
+
+        // 便于观察
+        Thread.sleep(3000000);
+    }
+
+    @Test
+    public void testHaveParamsTask() throws InterruptedException {
+        SchedulingRunnable task = new SchedulingRunnable("demoTask", "taskWithParams", "haha", 23);
+        cronTaskRegistrar.addCronTask(task, "0/10 * * * * ?");
+
+        // 便于观察
+        Thread.sleep(3000000);
+    }
+}
